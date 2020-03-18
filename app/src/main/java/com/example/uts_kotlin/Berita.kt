5eb76c4bf86c96d68b42166ff4970f3819d5e786
Bahar Yusuf Zakaria <57081313+Baharsky@users.noreply.github.com>
2020-03-18 14:43:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uts_kotlin/Berita.kt
@@ -0,0 +1,8 @@
+package com.example.uts_kotlin
+
+class Berita (
+    val judul:String,
+    val waktu:String,
+    val penulis:String,
+    val isi:String
+)
---
+++ app/src/main/java/com/example/uts_kotlin/BeritaActivity.kt
@@ -0,0 +1,49 @@
+package com.example.uts_kotlin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.common.Priority
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.JSONArrayRequestListener
+import kotlinx.android.synthetic.main.activity_berita.*
+import org.json.JSONArray
+
+
+class BeritaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_berita)
+
+
+        btn_save.setOnClickListener{
+            val judul = editTextJudul.text.toString()
+            val waktu = editTextWaktu.text.toString()
+            val penulis = editTextPenulis.text.toString()
+            val isi = editTextIsi.text.toString()
+            postServer(judul, waktu, penulis, isi)
+            Log.i("astaghfirullah",judul+waktu+penulis+isi)
+            startActivity(Intent(this, DashboardActivity::class.java))
+        }
+    }
+
+    private fun postServer(data1: String, data2: String, data3: String, data4: String) {
+        Log.i("astaghfirullah",data1+data2+data3)
+        AndroidNetworking.post("http://$ip/uts_kotlin/proses_input.php")
+            .addBodyParameter("judul_berita", data1)
+            .addBodyParameter("waktu_berita", data2)
+            .addBodyParameter("penulis_berita", data3)
+            .addBodyParameter("isi_berita", data4)
+            .setPriority(Priority.MEDIUM).build()
+            .getAsJSONArray(object : JSONArrayRequestListener {
+                override fun onResponse(response: JSONArray) {
+                }
+                override fun onError(anError: ANError?) {
+                    Log.i("_err", anError.toString())
+                }
+            })
+    }
+}
